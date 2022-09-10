3cb8fa10311d9bd9fb44bfb9edcc8fb9b658a93d
Alexey Larionov <alxs.larionov@gmail.com>
2022-09-10 23:03:56 +0300

    Working OBJ parsing for glDrawElements

--- src/common/obj_loader.py
+++ src/common/obj_loader.py
@@ -3,6 +3,7 @@ logger = logging.getLogger(__file__)
 
 import numpy as np
 import re
+from typing import Tuple, List, Dict, Any
 
 COMMENT_REGEXP = re.compile(r'#[^\n]*\n')
 FLOAT_REGEXP  = re.compile(r'(?:\s+(-?\d*\.?\d*))')
@@ -26,30 +27,40 @@ class ParsedWavefront:
 
     > import obj_loader
     > scene = obj_loader.ParsedWavefront('path/to/scene.obj')
-    > attributes   = scene.attributes_as_numpy('P3_T2')
-    > face_indices = scene.faces_as_numpy()
+    > attributes = scene.as_numpy('P3_T2')
+    > # or
+    > attributes, face_indices = scene.as_numpy_indexed('N2_P2_T1')
 
     """
 
     def __init__(self, wavefront_filepath: str, parse=True):
+        """wavefront_filepath: should be an existing file path to a Wavefront file
+           parse: if True the long parsing operation will be run immediately"""
         self.filepath = wavefront_filepath
         if parse:
             self.parse()
 
     def parse(self):
+        """Run the long executing parsing operation"""
         with open(self.filepath, 'r') as f:
-            p, t, n = ParsedWavefront.parse_string(f.read())
-        self.positions, self.positions_indices = p
-        self.texture_coordinates, self.texture_coordinates_indices = t
-        self.normals, self.normals_indices = n
-
+            self.parsed = ParsedWavefront.parse_string(f.read())
 
     @staticmethod
-    def parse_string(wavefront_str: str):
-        positions          , positions_indices           = [], []
-        texture_coordinates, texture_coordinates_indices = [], []
-        normals            , normals_indices             = [], []
+    def parse_string(wavefront_str: str) -> Dict[str, Any]:
+        """Extracts data from a multiline string that follows Wavefront (OBJ) file format"""
 
+        # data for glDrawArrays
+        positions_parsed, positions_array_indices = [], []
+        texcoords_parsed, texcoords_array_indices = [], []
+        normals_parsed  , normals_array_indices   = [], []
+
+        # data for glDrawElements
+        positions_indices, texcoords_indices, normals_indices = [], [], []
+        face_vertex_indices = []
+        current_vertex_index = 0
+        vertex_index_cache = {}
+
+        # remove comments from parsing
         wavefront_str = COMMENT_REGEXP.sub('\n', wavefront_str)
 
         for line_idx, line in enumerate(wavefront_str.splitlines()):
@@ -59,21 +70,35 @@ class ParsedWavefront:
 
             if line.startswith('vt'): # vertex texture coordinates
                 attribute_floats = tuple(map(float, FLOAT_REGEXP.findall(line)))
-                texture_coordinates.append(attribute_floats)
+                texcoords_parsed.append(attribute_floats)
             elif line.startswith('vn'): # vertex normals
                 attribute_floats = tuple(map(float, FLOAT_REGEXP.findall(line)))
-                normals.append(attribute_floats)
+                normals_parsed.append(attribute_floats)
             elif line.startswith('v'): # vertex positions
                 attribute_floats = tuple(map(float, FLOAT_REGEXP.findall(line)))
-                positions.append(attribute_floats)
+                positions_parsed.append(attribute_floats)
             elif line.startswith('f'): # face
-                # helping function to parse 1 vertex indices which may be in fomrs:
+                # helping function to parse indices of 1 vertex, which may be in forms:
                 # 'a/b/c', 'a//c', 'a/b', 'a'
                 def add_vertex_indices(*regexp_groups):
                     v, t, n = match.group(*regexp_groups)
-                    positions_indices.append(int(v)-1)
-                    if t: texture_coordinates_indices.append(int(t)-1)
-                    if n: normals_indices.append(int(n)-1)
+                    v = int(v) - 1
+                    positions_array_indices.append(v)
+                    if t:
+                        t = int(t) - 1
+                        texcoords_array_indices.append(t)
+                    if n:
+                        n = int(n) - 1
+                        normals_array_indices.append(n)
+                    vertex_index = vertex_index_cache.get((v,t,n))
+                    if not vertex_index:
+                        nonlocal current_vertex_index
+                        vertex_index_cache[(v,t,n)] = vertex_index = current_vertex_index
+                        current_vertex_index += 1
+                        positions_indices.append(v)
+                        if t is not None: texcoords_indices.append(t)
+                        if n is not None: normals_indices.append(n)
+                    face_vertex_indices.append(vertex_index)
 
 
                 match = FACE_REGEXP.match(line)
@@ -95,26 +120,84 @@ class ParsedWavefront:
 
         # check that either for all faces a certain attribute is defined,
         # or for all faces the attribute is undifined (missing)
-        if len(texture_coordinates_indices) not in [0, len(positions_indices)] or \
-           len(normals_indices)             not in [0, len(positions_indices)]:
+        if len(texcoords_array_indices) not in [0, len(positions_array_indices)] or \
+           len(normals_array_indices)   not in [0, len(positions_array_indices)]:
             raise Exception(f'Inconsistent faces definition"')
 
-        return (positions, positions_indices), \
-               (texture_coordinates, texture_coordinates_indices), \
-               (normals, normals_indices)
+        parse_result = dict(
+            positions_parsed        = positions_parsed,
+            positions_array_indices = positions_array_indices,
+            positions_indices       = positions_indices,
+            texcoords_parsed        = texcoords_parsed,
+            texcoords_array_indices = texcoords_array_indices,
+            texcoords_indices       = texcoords_indices,
+            normals_parsed          = normals_parsed,
+            normals_array_indices   = normals_array_indices,
+            normals_indices         = normals_indices,
+            face_vertex_indices     = face_vertex_indices,
+        )
+        return parse_result
 
+    def as_numpy(self, attributes_layout: str, dtype=np.float32) -> np.array:
+        """Returns an interleaved NumPy array of attributes (OpenGL ARRAY_BUFFER used with glDrawArrays) """
+        p = self.parsed['positions_array_indices']
+        t = self.parsed['texcoords_array_indices']
+        n = self.parsed['normals_array_indices']
+        interleaved_data = self.__make_interleaved_attributes(
+            attributes_layout, dtype=dtype,
+            positions_indices=p, texcoords_indices=t, normals_indices=n)
 
+        return interleaved_data
 
-    def attributes_as_numpy(self, attributes_layout: str, dtype=np.float32) -> np.array:
+    def as_numpy_indexed(self, attributes_layout: str, attrib_dtype=np.float32, indices_dtype=np.uint32) -> Tuple[np.array, np.array]:
+        """Returns an interleaved NumPy array of attributes (OpenGL ARRAY_BUFFER) and respective
+           index array which indexes the attributes array to define triangles (OpenGL ELEMENT_ARRAY_BUFFER)
+           This pair can be used for OpenGL glDrawElements call"""
+        p = self.parsed['positions_indices']
+        t = self.parsed['texcoords_indices']
+        n = self.parsed['normals_indices']
+        interleaved_data = self.__make_interleaved_attributes(
+            attributes_layout, dtype=attrib_dtype,
+            positions_indices=p, texcoords_indices=t, normals_indices=n)
+
+        face_vertex_indices = np.array(self.parsed['face_vertex_indices'], dtype=indices_dtype)
+
+        return interleaved_data, face_vertex_indices
+
+    def __make_interleaved_attributes(self, attributes_layout, positions_indices, texcoords_indices, normals_indices, dtype) -> np.array:
+        """
+        Parsed vertex attributes arrays may contain a different number of entries
+        Given `positions_indices`, `texcoords_indices`, `normals_indices` of the same length,
+        this function builds 1 numpy array with interleaved data for each vertex.
+        For example (here P1 may mean 3 numbers (x,y,z))
+        parsed: positions texcoords normals
+                    P0       T0        N0
+                    P1       T1        N1
+                    P2                 N2
+                    P3
+
+        indices:    0        0         2
+                    1        0         2
+                    2        1         1
+                    3        1         0
+                    0        0         1
+        and layout string: P3_N3_T2,
+        the function constructs an array with the structure
+                    P0       N3        T0
+                    P1       N2        T0
+                    P2       N1        T1
+                    P3       N0        T1
+                    P0       N1        T0
+        """
         parts = parse_interleaved_layout(attributes_layout)
-        attributes = {}
 
-        if self.positions_indices:
-            attributes['P'] = np.array(self.positions)[self.positions_indices]
-        if self.texture_coordinates_indices:
-            attributes['T'] = np.array(self.texture_coordinates)[self.texture_coordinates_indices]
-        if self.normals_indices:
-            attributes['N'] = np.array(self.normals)[self.normals_indices]
+        attributes = {}
+        if positions_indices:
+            attributes['P'] = np.array(self.parsed['positions_parsed'])[positions_indices]
+        if texcoords_indices:
+            attributes['T'] = np.array(self.parsed['texcoords_parsed'])[texcoords_indices]
+        if normals_indices:
+            attributes['N'] = np.array(self.parsed['normals_parsed'])[normals_indices]
 
         arrays_to_stack = []
         for key, used_coordinates in parts:
@@ -125,12 +208,13 @@ class ParsedWavefront:
 
         interleaved_data = np.ascontiguousarray(
             np.hstack(arrays_to_stack), dtype=dtype)
-
         return interleaved_data
 
-def parse_interleaved_layout(layout_str):
-    """ Example string: 'P3_T2_N3'
-    meaning 3 position values, 2 texture coordinates, 3 normal values"""
+def parse_interleaved_layout(layout_str: str) -> List[Tuple[str, int]]:
+    """ Parses the layout string into an array of tokens
+        Example string: 'P3_T2_N3'
+        Returns: [('P', 3), ('T', 2), ('N', 3)]
+        meaning 3 position values, 2 texture coordinates, 3 normal values"""
     parts = layout_str.upper().split('_')
     parsed_parts = []
     added_parts = set()
@@ -158,16 +242,18 @@ if __name__ == "__main__":
 
     print('Testing spot_control_mesh.obj')
     scene = ParsedWavefront('assets/spot_cow/spot_control_mesh.obj')
-    attributes   = scene.attributes_as_numpy('T1_P3')
-    # face_indices = scene.faces_as_numpy()
-    # print(face_indices)
-    # print(face_indices.shape)
+    attributes               = scene.as_numpy('T1_P3')
+    print('Attributes', attributes)
+    print(attributes.shape)
+    attributes, face_indices = scene.as_numpy_indexed('T1_P3')
+    print('Indexed attributes', attributes)
+    print(attributes.shape, 'indices:', face_indices.shape)
 
     print('Testing head.obj')
     scene = ParsedWavefront('assets/human_head/head.obj')
-    attributes   = scene.attributes_as_numpy('P4_T1')
+    attributes   = scene.as_numpy('P4_T1')
     print(attributes)
     print(attributes.shape)
-    # face_indices = scene.faces_as_numpy()
-    # print(face_indices)
-    # print(face_indices.shape)
+    attributes, face_indices = scene.as_numpy_indexed('P4_T1')
+    print('Indexed attributes', attributes)
+    print(attributes.shape, 'indices:', face_indices.shape)
