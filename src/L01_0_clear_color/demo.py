287b06b17399cee93af0125570a0dcb91daad878
Alexey Larionov <alxs.larionov@gmail.com>
2022-09-11 16:26:48 +0300

    Make the first demo less epileptically dangerous

--- src/L01_0_clear_color/demo.py
+++ src/L01_0_clear_color/demo.py
@@ -10,9 +10,9 @@ class Lecture01_ColorDemo(Demo):
         self.is_loaded = True
 
     def render_frame(self, width, height, global_time_sec, delta_time_sec):
-        red   = 0.5*math.sin(global_time_sec) + 0.5
-        green = 0.5*math.cos(global_time_sec*1.3) + 0.5
-        blue  = 0.5
+        red   = 0.3*math.sin(global_time_sec) + 0.3
+        green = 0.3*math.cos(global_time_sec*1.3) + 0.3
+        blue  = 0.3
         glClearColor(red,green,blue,1)
         glClear(GL_COLOR_BUFFER_BIT)
 
